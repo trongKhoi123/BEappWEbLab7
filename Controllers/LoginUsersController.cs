100bf03b2a57f2a069ee15fce2b4daf952f7b8fb
trongKhoi123 <145261663+trongKhoi123@users.noreply.github.com>
2023-11-01 18:22:46 +0700

    Add files via upload

---
+++ Controllers/LoginUsersController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BeWebApp.Models;
+
+namespace BeWebApp.Controllers
+{
+    public class LoginUsersController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+
+        // GET: LoginUsers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LoginUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminUser adminUser = db.AdminUsers.Find(id);
+            if (adminUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminUser);
+        }
+
+        // GET: LoginUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NameUser,RoleUser,PasswordUser")] AdminUser adminUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AdminUsers.Add(adminUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(adminUser);
+        }
+
+        // GET: LoginUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminUser adminUser = db.AdminUsers.Find(id);
+            if (adminUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminUser);
+        }
+
+        // POST: LoginUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NameUser,RoleUser,PasswordUser")] AdminUser adminUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adminUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(adminUser);
+        }
+
+        // GET: LoginUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminUser adminUser = db.AdminUsers.Find(id);
+            if (adminUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adminUser);
+        }
+
+        // POST: LoginUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AdminUser adminUser = db.AdminUsers.Find(id);
+            db.AdminUsers.Remove(adminUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult LoginAcount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginAcount(AdminUser _user)
+        {
+            var check = database.AdminUsers.Where(s => s.ID == user.ID && s.PasswordUser == user.PasswordUser).FirstOrDefault();
+            if (check == null) //login sai thong tin
+            {
+                ViewBag.ErrorInfo = "SaiInfo";
+                return View("Index");
+            }
+            else
+            {
+                database.Configuration.ValidateOnSaveEnabled = false;
+                Session["ID"] = _user.ID;
+                Session["PasswordUser"] = _user.PasswordUser;
+                return RedirectToAction("Index", "Product");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BeWebApp.Models;
+using System.IO;
+
+namespace BeWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category1);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,NamePro,DecriptionPro,Category,Price,ImagePro")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate", product.Category);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate", product.Category);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,NamePro,DecriptionPro,Category,Price,ImagePro")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate", product.Category);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult UploadProduct()
+        {
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadProduct([Bind(Include = "ProductID, NamePro, DecriptionPro, Category, Price, ImagePro, UploadImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+ 
+                if (product.UploadImage != null)
+                {
+                    string filename = Path.GetFileName(product.UploadImage.FileName);
+                    product.ImagePro = "/Content/images/" + filename;
+                    product.UploadImage.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), filename));
+                }
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category = new SelectList(db.Categories, "IDCate", "NameCate", product.Category);
+            return View(product);
+        }
+
+    }
+}
